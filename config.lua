8de9d817cae604778273d02811b391ba136374cb
Torch ICO <tommyrpuk@gmail.com>
2024-03-20 05:27:42 +0000

    Fixed framework in Config

--- config.lua
+++ config.lua
@@ -1,3 +1,3 @@
 Config = {}
-Config.Banking.Framework = 'qbx' -- Banking method, accepts 'qbx' for Renewed Banking, 'pefcl' for Project Error's PEFCL & 'custom' for your own banking methodology. (Modify the server/transactions.lua)
+Config.Framework = 'qbx' -- Banking method, accepts 'qbx' for Renewed Banking, 'pefcl' for Project Error's PEFCL & 'custom' for your own banking methodology. (Modify the server/transactions.lua)
 Config.NotificationLength = 7500 -- Length of time Phone notifications display
