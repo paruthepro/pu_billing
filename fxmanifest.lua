910488de14bc0e62bed7ed41820475343356ae44
Torch ICO <tommyrpuk@gmail.com>
2024-03-20 04:28:42 +0000

    chore: Version bump

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ lua54 'yes'
 
 author 'Paru'
 description 'A generic Ox Lib based Billing resource created by Paru.'
-version '1.1'
+version '1.2'
 
 shared_scripts {
     '@qbx_core/modules/playerdata.lua',
