bd03f5a9c9431f44df46ec0500d1430d1c5e062d
Torch ICO <tommyrpuk@gmail.com>
2024-03-20 14:05:08 +0000

    Added alternative to phone support

--- client/main.lua
+++ client/main.lua
@@ -10,29 +10,63 @@ RegisterNetEvent('pu_billing:client:bill', function()
 end)
 
 RegisterNetEvent('pu_billing:client:receiveBill', function(amount, job, token)
-    exports["npwd"]:createSystemNotification({
-        uniqId = "Pu_billing",
-        content = ("Do you accept the bill? ($%s)"):format(amount),
-        secondaryTitle = ("From %s"):format(job),
-        keepOpen = true,
-        duration = Config.NotificationLength,
-        controls = true,
-        onConfirm = function()
+    if Config.NPWD then
+        exports["npwd"]:createSystemNotification({
+            uniqId = "Pu_billing",
+            content = ("Do you accept the bill? ($%s)"):format(amount),
+            secondaryTitle = ("From %s"):format(job),
+            keepOpen = true,
+            duration = Config.NotificationLength,
+            controls = true,
+            onConfirm = function()
+                TriggerServerEvent("pu_billing:server:reply", token, true)
+            end,
+            onCancel = function()
+            TriggerServerEvent("pu_billing:server:reply", token, false)
+            end,
+        })
+    else
+        local bill = lib.alertDialog({
+            header = 'Billing Portal',
+            content = ('Do you accept the bill? ($%s)\n From'.." "..job):format(amount),
+            centered = true,
+            cancel = true,
+            labels = {
+                cancel = 'Decline Bill',
+                confirm = 'Accept Bill'
+            }
+        })
+        if bill == 'confirm' then
             TriggerServerEvent("pu_billing:server:reply", token, true)
-        end,
-        onCancel = function()
-          TriggerServerEvent("pu_billing:server:reply", token, false)
-        end,
-      })
+        else
+            TriggerServerEvent("pu_billing:server:reply", token, false)
+        end
+    end
 end)
 
 RegisterNetEvent('pu_billing:client:receiveBillResponse', function(accepted, name, job, amount)
-    exports["npwd"]:createSystemNotification({
-        uniqId = accepted and "Pu_billing_paid" or "Pu_billing_rejected",
-        content = accepted and ("Bill paid by %s at %s for $%s"):format(name, job, amount) or ("Bill rejected by %s"):format(name),
-        secondaryTitle = ("from %s"):format(job),
-        keepOpen = false,
-        duration = Config.NotificationLength,
-        controls = false,
-    })
+    if Config.NPWD then
+        exports["npwd"]:createSystemNotification({
+            uniqId = accepted and "Pu_billing_paid" or "Pu_billing_rejected",
+            content = accepted and ("Bill paid by %s at %s for $%s"):format(name, job, amount) or ("Bill rejected by %s"):format(name),
+            secondaryTitle = ("from %s"):format(job),
+            keepOpen = false,
+            duration = Config.NotificationLength,
+            controls = false,
+        })
+    else
+        local bill = lib.alertDialog({
+            header = 'Billing Portal',
+            content = ('The bill sent to'.." "..name.." "..'has been cancelled'..'\n Total Cost: '..""..amount):format(amount),
+            centered = true,
+            cancel = true,
+            labels = {
+                cancel = 'Rebill',
+                confirm = 'Close'
+            }
+        })
+        if bill == 'cancel' then
+            TriggerEvent('pu_billing:client:bill')
+        end
+    end
 end)
--- config.lua
+++ config.lua
@@ -1,3 +1,4 @@
 Config = {}
 Config.Framework = 'qbx' -- Banking method, accepts 'qbx' for Renewed Banking, 'pefcl' for Project Error's PEFCL & 'custom' for your own banking methodology. (Modify the server/transactions.lua)
 Config.NotificationLength = 7500 -- Length of time Phone notifications display
+Config.NPWD = true -- This is used for adjusting the billing notifications, if set to false will utilise lib.alertDialog instead.
